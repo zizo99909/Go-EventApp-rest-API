a729603adbb413fe9d81065cd8cce9de4671f1c9
CodeSandbox <support@codesandbox.io>
2025-02-18 15:43:57 +0000

    Sandbox snapshot commit

--- db/db.go
+++ db/db.go
@@ -5,6 +5,18 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var DB *sql.DB
+
 func InitDB() {
-	sql.Open("sqlite3", "api.db")
+	var err error
+	DB, err = sql.Open("sqlite3", "api.db")
+
+	if err != nil {
+		panic("Could not connect to database.")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+
+	createTables()
 }
