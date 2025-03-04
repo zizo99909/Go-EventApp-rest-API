eb8a64ffd128a50db9c892fda6fd32b3391410bc
CodeSandbox <support@codesandbox.io>
2025-03-04 10:48:23 +0000

    Sandbox snapshot commit

--- db/db.go
+++ db/db.go
@@ -57,7 +57,16 @@ func createTables() {
 
 	createRegistrationsTable := `
 	CREATE TABLE IF NOT EXISTS registrations (
-		id INTEGER PRIMARY KEY AUTOINCREMENT
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		event_id INTEGER,
+		user_id INTEGER,
+		FOREIGN KEY(event_id) REFERENCES events(id),
+		FOREIGN KEY(user_id) REFERENCES users(id)
 	)
 	`
+	_, err = DB.Exec(createRegistrationsTable)
+
+	if err != nil {
+		panic("Could not create events table.")
+	}
 }
