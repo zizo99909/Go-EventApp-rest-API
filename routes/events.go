49a6023d04877c2c47f8cd19735a59b8a6d6bdb3
CodeSandbox <support@codesandbox.io>
2025-03-04 09:25:23 +0000

    Sandbox snapshot commit

--- routes/events.go
+++ routes/events.go
@@ -110,10 +110,16 @@ func deleteEvent(context *gin.Context) {
 		return
 	}
 
+	userId := context.GetInt64("userId")
+
 	event, err := models.GetEventByID(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
+	}
+
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update event."})
 		return
 	}
 
