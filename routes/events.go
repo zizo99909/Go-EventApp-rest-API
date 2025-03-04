b600b4b32b4c4e06c38da0e8c3e77cc0ccabe17c
CodeSandbox <support@codesandbox.io>
2025-03-04 09:04:44 +0000

    Sandbox snapshot commit

--- routes/events.go
+++ routes/events.go
@@ -68,12 +68,18 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 
-	_, err = models.GetEventByID(eventId)
+	userId := context.GetInt64("userId")
+
+	event, err := models.GetEventByID(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch the event."})
 	}
 
+	if event.UserID != userId {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized to update event."})
+		return
+	}
 	var updatedEvent models.Event
 
 	err = context.ShouldBindJSON(&updatedEvent)
